f37d555550c92d84bd3a4328e4f0500677c819c8
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-07-25 10:33:26 +0900

    indent

--- plus.py
+++ plus.py
@@ -3,12 +3,12 @@ import os, sys, math
 
 
 if __name__=='__main__':
-	if len(sys.argv) <= 2:
-	    print("Please specify more than two paths of the number images you want to plus in this program as arguments.")
-	    exit()
+    if len(sys.argv) <= 2:
+        print("Please specify more than two paths of the number images you want to plus in this program as arguments.")
+        exit()
 
-	result=0
-	for i in range(len(sys.argv)-1):
-		result+=pd.predict_digit(sys.argv[i+1])
+    result=0
+    for i in range(len(sys.argv)-1):
+        result+=pd.predict_digit(sys.argv[i+1])
 
-	print("RESULT:",result)
+    print("RESULT:",result)
--- predict_digit.py
+++ predict_digit.py
@@ -9,43 +9,43 @@ import os, sys, math
 
 # 画像から数字を予測し、その数字を返す関数
 def predict_digit(imagefile):
-	DIGITS_PKL = "./model/digit-clf.pkl"
+    DIGITS_PKL = "./model/digit-clf.pkl"
 
-	# 画像を8×8に変換する処理
-	image = Image.open(imagefile).convert('L')
-	image = image.resize((8, 8), Image.ANTIALIAS)
-	img = np.asarray(image, dtype=float)
-	img = np.floor(16 - 16 * (img / 256))
-	# 変換された画像を表示する場合はコメントアウトを解除する
-	# plt.imshow(img)
-	# plt.gray()
-	# plt.show()
-	img = img.flatten()
+    # 画像を8×8に変換する処理
+    image = Image.open(imagefile).convert('L')
+    image = image.resize((8, 8), Image.ANTIALIAS)
+    img = np.asarray(image, dtype=float)
+    img = np.floor(16 - 16 * (img / 256))
+    # 変換された画像を表示する場合はコメントアウトを解除する
+    # plt.imshow(img)
+    # plt.gray()
+    # plt.show()
+    img = img.flatten()
 
-	# モデルが作成されてない場合は作る
-	if not os.path.exists(DIGITS_PKL):
-	    digits = datasets.load_digits()
+    # モデルが作成されてない場合は作る
+    if not os.path.exists(DIGITS_PKL):
+        digits = datasets.load_digits()
 
-	    # 訓練
-	    data_train = digits.data
-	    label_train = digits.target
-	    clf = svm.SVC(gamma=0.001)
-	    clf.fit(data_train, label_train)
+        # 訓練
+        data_train = digits.data
+        label_train = digits.target
+        clf = svm.SVC(gamma=0.001)
+        clf.fit(data_train, label_train)
 
-	    # 予測モデルを保存
-	    joblib.dump(clf, DIGITS_PKL)
+        # 予測モデルを保存
+        joblib.dump(clf, DIGITS_PKL)
 
-	clf = joblib.load(DIGITS_PKL)
-	n = clf.predict([img])
-	return n[0]
+    clf = joblib.load(DIGITS_PKL)
+    n = clf.predict([img])
+    return n[0]
 
 
 
 if __name__=='__main__':
-	if len(sys.argv) <= 1:
-	    print("Please specify the path of the number image you want to predict in this program as second argument.")
-	    exit()
+    if len(sys.argv) <= 1:
+        print("Please specify the path of the number image you want to predict in this program as second argument.")
+        exit()
 
-	imagefile=sys.argv[1]
-	result=predict_digit(imagefile)
-	print("RESULT:",result)
+    imagefile=sys.argv[1]
+    result=predict_digit(imagefile)
+    print("RESULT:",result)
